73067a19935dd3845d98ffd5338b602af6ad24c0
Abeladmassu16 <abeladmassu3097@gmail.com>
2024-04-01 14:39:40 +0300

    update

--- my-song-app/src/App.test.js
+++ my-song-app/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Song from './song';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<Song />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
--- my-song-app/src/sagas.js
+++ my-song-app/src/sagas.js
@@ -11,3 +11,16 @@ export default function* rootSaga() {
     // Add other feature sagas here
   ]);
 }
+// src/app/sagas.js
+import { all } from 'redux-saga/effects';
+
+// Import sagas from features
+import { songsSaga } from '../features/songs/sagas';
+
+// Root saga to combine all feature sagas
+export default function* rootSaga() {
+  yield all([
+    songsSaga(),
+    // Add other feature sagas here
+  ]);
+}
