4a11becb33c9e00ee15acf25ae7ce6cbeb8ba6ef
Abeladmassu16 <abeladmassu3097@gmail.com>
2024-04-01 14:28:22 +0300

    edited

---
+++ my-song-app/src/sagas.js
@@ -0,0 +1,13 @@
+// src/app/sagas.js
+import { all } from 'redux-saga/effects';
+
+// Import sagas from features
+import { songsSaga } from '../features/songs/sagas';
+
+// Root saga to combine all feature sagas
+export default function* rootSaga() {
+  yield all([
+    songsSaga(),
+    // Add other feature sagas here
+  ]);
+}
